9533b3d24f73036b9d8aa42f06686d2139fdb131
Graitavicius <grait.mindaugas@gmail.com>
2020-06-19 00:02:32 +0300

    getting all products

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Table, Button } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+  state = {
+    products: []
+  }
+  
+  componentWillMount() {
+    axios.get('http://localhost:3000/products')
+      .then(response => {
+        this.setState({
+          products: response.data.products,
+        })
+      });
+  }
+
+  
+
+  
+  render () {
+    let products = this.state.products.map((product) => {
+      return (
+        <tr key={product._id}>
+          <td>{product.count}</td>
+          <td>{product._id}</td>
+          <td>{product.name}</td>
+          <td>{product.price}</td>
+          <td>{product.productImage}</td>
+          <td>
+            <Button color="success" size="sm" className="mr-2">Edit</Button>
+            <Button color="danger" size="sm">Delete</Button>
+          </td>
+        </tr>
+      )
+    });
+    return (
+      <div className="App container">
+        <Table>
+          <thead>
+            <tr>
+              <th>Amount</th>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Image</th>
+              <th>Actions</th>
+              
+            </tr>
+          </thead>
+
+          <tbody>
+            { products }
+          </tbody>
+
+
+        </Table>
+      </div>
+    )
+  }
+
 }
 
 export default App;
